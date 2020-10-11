0964a0a506263eb238b818e44a05e624b56d5609
Cheng <chengjohnsonhs@gmail.com>
2020-10-11 23:51:57 +0800

    import eslintWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-console */
 import data from "./data.json";
 import "./styles/index.css";
 import "./styles/index.less";
 import "./assets/iconFonts/flaticon.css";
 
-const add = (x, y) => {
-  return x + y;
-};
+const add = (x, y) => x + y;
 
 console.log(add(1, 2));
 console.log(data);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 // process.env.NODE_ENV = "development";
 
@@ -11,63 +12,63 @@ module.exports = {
     filename: "js/built.js",
     path: path.resolve(__dirname, "build"),
   },
-  //loader
+  // loader
   module: {
     rules: [
       {
         // 符合哪些文件
         test: /\.(css|less)$/,
-        //multiple loader using 'use:[]'
+        // multiple loader using 'use:[]'
         use: [
-          //use loader, compile order from right to left (from bottom to top)
-          //inject styleTag to the DOM
+          // use loader, compile order from right to left (from bottom to top)
+          // inject styleTag to the DOM
           // "style-loader", // 2
-          //replace style-loader, extract css from .js file
+          // replace style-loader, extract css from .js file
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
               publicPath: "../",
             },
           },
-          //將 css 轉成 commonjs module 載入 js 中，內容為 string
+          // 將 css 轉成 commonjs module 載入 js 中，內容為 string
           "css-loader", // 1
-          {
-            // css compatible using postcss -> postcss-loader, postcss-preset-env
-            loader: "postcss-loader",
-            options: {
-              //postcss 的 plugin
-              //help postcss find the configuration of browerslist in package.json
-              postcssOptions: {
-                plugins: ["postcss-preset-env"],
-              },
-            },
-          },
+          // {
+          //   // css compatible using postcss -> postcss-loader, postcss-preset-env
+          //   loader: "postcss-loader",
+          //   options: {
+          //     //postcss 的 plugin
+          //     //help postcss find the configuration of browerslist in package.json
+          //     postcssOptions: {
+          //       plugins: ["postcss-preset-env"],
+          //     },
+          //   },
+          // },
           { loader: "less-loader" },
         ],
       },
       {
         test: /\.(jpg|png|gif)$/,
         // just one loader, using "loader" directly
-        //download url-loader file-loader
-        loader: "url-loader", //compress file, compare to file-loader
+        // download url-loader file-loader
+        loader: "url-loader", // compress file, compare to file-loader
         options: {
           limit: 8 * 1024,
           // turn off es6 module, use commonjs (html-loader)
           esModule: false,
-          //[hash:10] 取 hash 前 10 位
-          //[ext] 原檔名
+          // [hash:10] 取 hash 前 10 位
+          // [ext] 原檔名
           name: "[hash:10].[ext]",
           outputPath: "imgs",
         },
       },
       {
         test: /\.html$/,
-        //handle image in .html, then can be load by url-loader
+        // handle image in .html, then can be load by url-loader
         loader: "html-loader",
         // outputPath: "medias",
       },
       {
-        //打包其他資源（html/js/css以外的資源）
+        // 打包其他資源（html/js/css以外的資源）
         exclude: /\.(css|html|js|less|json|jpg|png|gif)$/,
         loader: "file-loader",
         options: {
@@ -84,20 +85,23 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "css/built.css",
     }),
-    //compress css
+    // compress css
     new OptimizeCssAssetsPlugin(),
+    new ESLintPlugin({
+      fix: true,
+    }),
   ],
-  // mode: "development",
+  mode: "development",
 
-  //devServer: auto compile, launch browser, auto refresh... (live server)
-  //no output file
+  // devServer: auto compile, launch browser, auto refresh... (live server)
+  // no output file
   devServer: {
-    //build directory
+    // build directory
     contentBase: path.resolve(__dirname, "build"),
-    //gzip compress
+    // gzip compress
     compress: true,
     port: 4000,
-    //auto launch browser
+    // auto launch browser
     open: true,
   },
 };
