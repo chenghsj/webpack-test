632dac81fbb320b8af4214d3587612e866684072
Cheng <chengjohnsonhs@gmail.com>
2020-10-18 00:06:36 +0800

    thread-loader

--- test3/webpack.config.js
+++ test3/webpack.config.js
@@ -44,27 +44,37 @@ module.exports = {
         test: /\.less$/,
         use: [...commonCssLoader, "less-loader"],
       },
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   loader: "babel-loader",
-      //   options: {
-      //     presets: [
-      //       [
-      //         "@babel/preset-env",
-      //         {
-      //           useBuiltIns: "usage",
-      //           corejs: 3.6,
-      //           targets: {
-      //             chrome: "60",
-      //             firefox: "50",
-      //           },
-      //         },
-      //       ],
-      //     ],
-      //     cacheDirectory: true,
-      //   },
-      // },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          // packed with multiple thread
+          // Each worker is a separate node.js process, which has an overhead
+          // of ~600ms.
+          // There is also an overhead of inter-process communication.
+          // Use the loader only for expensive operations
+          // "thread-loader",
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: 3.6,
+                    targets: {
+                      chrome: "60",
+                      firefox: "50",
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
       {
         test: /\.(jpg|png|gif)$/,
         loader: "url-loader",
